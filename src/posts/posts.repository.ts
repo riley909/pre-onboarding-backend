0c6377a1f4266f8b2f70d3c0a6cc217109e872f2
riley909 <increaseblue666@gmail.com>
2021-10-25 00:09:13 +0900

    add getPosts

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -1,5 +1,26 @@
+import { InternalServerErrorException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { GetPostsFilterDto } from './dto/get-posts-filter.dto';
 import { Post } from './post.entity';
 
 @EntityRepository(Post)
-export class PostsRepository extends Repository<Post> {}
+export class PostsRepository extends Repository<Post> {
+  async getPosts(filterDto: GetPostsFilterDto): Promise<Post[]> {
+    const { search } = filterDto;
+    const query = this.createQueryBuilder('post');
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(post.title) LIKE LOWER(:search) OR LOWER(post.content) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
+    try {
+      const posts = await query.getMany();
+      return posts;
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
+}
