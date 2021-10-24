88d5f4cb673222d753f6c6923d75360c08a37ef8
riley909 <increaseblue666@gmail.com>
2021-10-24 23:25:47 +0900

    inject PostsRepository

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostsRepository } from './posts.repository';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostsRepository)
+    private postsRepository: PostsRepository,
+  ) {}
+}
