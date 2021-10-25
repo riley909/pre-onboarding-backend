dedb0aee2eddc62478070c2265f0e299f9c71856
riley909 <increaseblue666@gmail.com>
2021-10-25 19:24:43 +0900

    add validationSchema to ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
