697765d88e3709507735a037c14eb99a65819dd7
riley909 <increaseblue666@gmail.com>
2021-10-25 16:08:37 +0900

    create AuthCredentialsDto

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: '비밀번호 규칙에 맞지 않습니다.',
+  })
+  password: string;
+}
