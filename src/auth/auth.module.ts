48f90b07ea07388f62998468c93ce60968bfbc96
riley909 <increaseblue666@gmail.com>
2021-10-25 17:51:36 +0900

    import PassportModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersRepository } from './users.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UsersRepository]),
+    PassportModule.register({}),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
