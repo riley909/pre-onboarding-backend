583beb64fec4d20c630ca6d5a4fc808c01f446b9
riley909 <increaseblue666@gmail.com>
2021-10-25 19:50:47 +0900

    import AuthModule

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { PostsController } from './posts.controller';
 import { PostsRepository } from './posts.repository';
 import { PostsService } from './posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostsRepository]), ConfigModule],
+  imports: [
+    TypeOrmModule.forFeature([PostsRepository]),
+    ConfigModule,
+    AuthModule,
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
