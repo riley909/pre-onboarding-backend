f6abcc19f29498b4b5da53c9460e8a5e59e92a66
riley909 <increaseblue666@gmail.com>
2021-10-26 09:01:21 +0900

    remove unused import

--- src/posts/posts.repository.ts
+++ src/posts/posts.repository.ts
@@ -3,7 +3,7 @@ import {
   InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
-import { EntityRepository, Like, Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PostStatus } from './post-status.enum';
