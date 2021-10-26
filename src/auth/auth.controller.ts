cbaae9cb6e17b83a0b44670aa5216c0a0dbfbcdb
riley909 <increaseblue666@gmail.com>
2021-10-26 10:09:18 +0900

    add Public decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { Public } from './decorator/public.decorator';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Controller('auth')
@@ -7,11 +8,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
+  @Public()
   signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
     return this.authService.signUp(authCredentialsDto);
   }
 
   @Post('/signin')
+  @Public()
   signIn(
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
